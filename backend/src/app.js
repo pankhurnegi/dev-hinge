cb2ed660b52785fd152ee2a80c0f8934ed5ee84b
pankhur <negipankhur@gmail.com>
2025-09-15 19:06:32 +0530

    error handling routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,16 +2,27 @@ const express = require("express");
 const app = express();
 
 
-//this will match all the HTTP method API calls to /test
-app.use("/test", (req, res) => {
-    res.send("This is a test!");
+
+
+app.get("/getUserData", (req, res, next) => {
+    try {
+
+        throw new Error("this is an error");
+        res.send("user data sent");
+
+    }
+    catch (err) {
+        res.status(500).send("something went wrong contact support team!");
+    }
 });
 
-app.use("/", (req, res) => {
-    res.send("Hello from the server!");
+
+app.use((err, req, res) => {
+
+    if (err) {
+        res.status(500).send("something went wrong");
+    }
 });
-
-
 
 app.listen(3000, () => {
     console.log("Server is successfully listening to port 3000...");
